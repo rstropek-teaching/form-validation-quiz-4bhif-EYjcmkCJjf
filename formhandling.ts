cbd20a2afed8a617b2f1af0791a6467f74144960
EYjcmkCJjf <32522511+EYjcmkCJjf@users.noreply.github.com>
2017-11-30 13:42:58 +0100

    Add files via upload
    Something didn't work when i tried to compile my TypeScript to JavaScript.
    And i want to apologise that it took me that long to do my homework.

---
+++ formhandling.ts
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    $("#firstName").change(function(){
+      if(hasValue("#firstName")){
+          $("firstNameMandatory").hide();
+      }else{
+          $("firstNameMandatory").show();
+      }
+    });
+    
+    $("#lastName").change(function(){
+        if(hasValue("#lastName")){
+            $("lastNameMandatory").hide();
+        }else{
+            $("lastNameMandatory").show();
+        }
+    });
+    
+    $("#email").change(function(){
+        if(hasValue("#email") && $("#newsletter").is(":checked")){
+            $("lastNameMandatory").hide();
+        }else{
+            $("lastNameMandatory").show();
+        }
+    });
+
+    if($("#mediaChannelSelect").val == "Other"){
+        $("otherMediaChannel").show();
+    }else{
+        $("otherMediaChannel").hide();
+    }
+    function hasValue(elem) {
+        return $(elem).filter(function() { return $(this).val(); }).length > 0;
+    }
+
+});
+
+
+
+
